0eceba5c56f3b70933258140d637730b0a9ee56a
light <373465009@qq.com>
2019-12-03 16:07:20 +0800

    Load global settings修改成从本地读取规则

--- sonar-scanner-engine/src/main/java/org/sonar/scanner/repository/settings/DefaultSettingsLoader.java
+++ sonar-scanner-engine/src/main/java/org/sonar/scanner/repository/settings/DefaultSettingsLoader.java
@@ -20,86 +20,134 @@
 package org.sonar.scanner.repository.settings;
 
 import com.google.common.annotations.VisibleForTesting;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import javax.annotation.Nullable;
+import com.google.protobuf.util.JsonFormat;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
 import org.sonar.api.utils.log.Profiler;
 import org.sonar.scanner.bootstrap.ScannerWsClient;
-import org.sonar.scanner.util.ScannerUtils;
 import org.sonarqube.ws.Settings.FieldValues.Value;
 import org.sonarqube.ws.Settings.Setting;
 import org.sonarqube.ws.Settings.ValuesWsResponse;
-import org.sonarqube.ws.client.GetRequest;
+
+import javax.annotation.Nullable;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DefaultSettingsLoader implements SettingsLoader {
 
-  private ScannerWsClient wsClient;
-  private static final Logger LOG = Loggers.get(DefaultSettingsLoader.class);
+    private ScannerWsClient wsClient;
+    private static final Logger LOG = Loggers.get(DefaultSettingsLoader.class);
 
-  public DefaultSettingsLoader(ScannerWsClient wsClient) {
-    this.wsClient = wsClient;
-  }
-
-  @Override
-  public Map<String, String> load(@Nullable String componentKey) {
-    String url = "api/settings/values.protobuf";
-    Profiler profiler = Profiler.create(LOG);
-    if (componentKey != null) {
-      url += "?component=" + ScannerUtils.encodeForUrl(componentKey);
-      profiler.startInfo("Load settings for component '" + componentKey + "'");
-    } else {
-      profiler.startInfo("Load global settings");
+    public DefaultSettingsLoader(ScannerWsClient wsClient) {
+        this.wsClient = wsClient;
     }
-    try (InputStream is = wsClient.call(new GetRequest(url)).contentStream()) {
-      ValuesWsResponse values = ValuesWsResponse.parseFrom(is);
-      profiler.stopInfo();
-      return toMap(values.getSettingsList());
-    } catch (IOException e) {
-      throw new IllegalStateException("Failed to load server settings", e);
-    }
-  }
 
-  @VisibleForTesting
-  static Map<String, String> toMap(List<Setting> settingsList) {
-    Map<String, String> result = new LinkedHashMap<>();
-    for (Setting s : settingsList) {
-      if (!s.getInherited()) {
-        switch (s.getValueOneOfCase()) {
-          case VALUE:
-            result.put(s.getKey(), s.getValue());
-            break;
-          case VALUES:
-            result.put(s.getKey(), s.getValues().getValuesList().stream().map(StringEscapeUtils::escapeCsv).collect(Collectors.joining(",")));
-            break;
-          case FIELDVALUES:
-            convertPropertySetToProps(result, s);
-            break;
-          default:
-            throw new IllegalStateException("Unknow property value for " + s.getKey());
+    @Override
+    public Map<String, String> load(@Nullable String componentKey) {
+        Profiler profiler = Profiler.create(LOG);
+        if (componentKey != null) {
+            throw new IllegalStateException("Load setting with component key is not supported.");
+        } else {
+            profiler.startInfo("Load global settings");
         }
-      }
+        try  {
+            String json = readFileContent("/Users/lightpan/code/java/json/global.json");
+            ValuesWsResponse.Builder builder = ValuesWsResponse.getDefaultInstance().toBuilder();
+            JsonFormat.parser().merge(json, builder);
+            ValuesWsResponse values = builder.build();
+            profiler.stopInfo();
+            return toMap(values.getSettingsList());
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to load server settings", e);
+        }
     }
-    return result;
-  }
 
-  private static void convertPropertySetToProps(Map<String, String> result, Setting s) {
-    List<String> ids = new ArrayList<>();
-    int id = 1;
-    for (Value v : s.getFieldValues().getFieldValuesList()) {
-      for (Map.Entry<String, String> entry : v.getValue().entrySet()) {
-        result.put(s.getKey() + "." + id + "." + entry.getKey(), entry.getValue());
-      }
-      ids.add(String.valueOf(id));
-      id++;
+    private String readFileContent(String fileName) {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        StringBuilder sbf = new StringBuilder();
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String tempStr;
+            while ((tempStr = reader.readLine()) != null) {
+                sbf.append(tempStr);
+            }
+            reader.close();
+            return sbf.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        return sbf.toString();
     }
-    result.put(s.getKey(), ids.stream().collect(Collectors.joining(",")));
-  }
+
+//  @Override
+//  public Map<String, String> load(@Nullable String componentKey) {
+//    String url = "api/settings/values.protobuf";
+//    Profiler profiler = Profiler.create(LOG);
+//    if (componentKey != null) {
+//      url += "?component=" + ScannerUtils.encodeForUrl(componentKey);
+//      profiler.startInfo("Load settings for component '" + componentKey + "'");
+//    } else {
+//      profiler.startInfo("Load global settings");
+//    }
+//    try (InputStream is = wsClient.call(new GetRequest(url)).contentStream()) {
+//      ValuesWsResponse values = ValuesWsResponse.parseFrom(is);
+//      profiler.stopInfo();
+//      return toMap(values.getSettingsList());
+//    } catch (IOException e) {
+//      throw new IllegalStateException("Failed to load server settings", e);
+//    }
+//  }
+
+    @VisibleForTesting
+    static Map<String, String> toMap(List<Setting> settingsList) {
+        Map<String, String> result = new LinkedHashMap<>();
+        for (Setting s : settingsList) {
+            if (!s.getInherited()) {
+                switch (s.getValueOneOfCase()) {
+                    case VALUE:
+                        result.put(s.getKey(), s.getValue());
+                        break;
+                    case VALUES:
+                        result.put(s.getKey(), s.getValues().getValuesList().stream().map(StringEscapeUtils::escapeCsv).collect(Collectors.joining(",")));
+                        break;
+                    case FIELDVALUES:
+                        convertPropertySetToProps(result, s);
+                        break;
+                    default:
+                        throw new IllegalStateException("Unknow property value for " + s.getKey());
+                }
+            }
+        }
+        return result;
+    }
+
+    private static void convertPropertySetToProps(Map<String, String> result, Setting s) {
+        List<String> ids = new ArrayList<>();
+        int id = 1;
+        for (Value v : s.getFieldValues().getFieldValuesList()) {
+            for (Map.Entry<String, String> entry : v.getValue().entrySet()) {
+                result.put(s.getKey() + "." + id + "." + entry.getKey(), entry.getValue());
+            }
+            ids.add(String.valueOf(id));
+            id++;
+        }
+        result.put(s.getKey(), ids.stream().collect(Collectors.joining(",")));
+    }
 }
